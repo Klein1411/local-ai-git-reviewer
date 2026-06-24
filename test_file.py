7724712788aa19b874b623ec49627d41c9cb290a
Tran Anh kiet <tranzeda123098@gmail.com>
2026-06-24 20:03:39 +0700

    chore: remove unused test_file.py

--- test_file.py
+++
@@ -1,8 +0,0 @@
-def calculate_sum(a, b):
-    # Lỗi cố ý: Tên hàm là tính tổng (sum) nhưng lại đi nhân (multiply)
-    return a * b
-
-def hello_world():
-    print("Hello world")
-    
-# Quên chưa gọi hàm
